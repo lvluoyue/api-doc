d0512e0a9929ae1e45c515d83fdfa57d9138e051
落月 <1569097443@qq.com>
2025-03-18 19:25:40 +0800

    fix pwa

--- .vitepress/theme/layout/RegisterSW.vue
+++ .vitepress/theme/layout/RegisterSW.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue'
-
-const offlineReady = ref(false)
-const needRefresh = ref(false)
+import { useWebNotification } from '@vueuse/core'
+const {isSupported,notification,show,close,onClick,onShow,onError,onClose} = useWebNotification({
+  title: 'Hello, VueUse world!',
+  dir: 'auto',
+  lang: 'en',
+  renotify: true,
+  tag: 'test',
+})
 
 let updateServiceWorker: (() => Promise<void>) | undefined
 
 function onOfflineReady() {
-  offlineReady.value = true
+  show()
 }
 function onNeedRefresh() {
-  needRefresh.value = true
-}
-async function close() {
-  offlineReady.value = false
-  needRefresh.value = false
+  show()
 }
 
 const intervalMS = 5 * 60 * 1000;
@@ -51,56 +52,7 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <template v-if="offlineReady || needRefresh">
-    <div
-        class="pwa-toast"
-        role="alertdialog"
-        aria-labelledby="pwa-message"
-    >
-      <div id="pwa-message" class="mb-3">
-        {{ offlineReady ? '应用程序已准备好离线工作' : '有新内容可用，点击重新加载按钮进行更新。' }}
-      </div>
-      <button
-          v-if="needRefresh"
-          type="button"
-          class="pwa-refresh"
-          @click="updateServiceWorker?.()"
-      >
-        更新
-      </button>
-      <button
-          type="button"
-          class="pwa-cancel"
-          @click="close"
-      >
-        关闭
-      </button>
-    </div>
-  </template>
 </template>
 
 <style>
-.pwa-toast {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 100;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
-}
-.pwa-toast #pwa-message {
-  margin-bottom: 8px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
 </style>
